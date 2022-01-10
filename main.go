76a11d440b4dba87ed63ce94493e87d01786dc86
Nowfel Mashnoor <nowfel.mashnoor@upaybd.com>
2022-01-10 19:55:56 +0600

    Init

--- main.go
+++ main.go
@@ -3,15 +3,74 @@ package main
 import (
 	"fmt"
 	"os/exec"
+
+	"strings"
+	"errors"
 )
 
-func main()  {
-	cmd := exec.Command("echo", "-e", "hello")
+func printOut(cmd *exec.Cmd) error  {
 	stdout, err := cmd.Output()
-
 	if err == nil {
 		fmt.Println(string(stdout))
+		if strings.Contains(string(stdout), "Error") {
+			return errors.New("empty name")
+		}
 	} else {
 		fmt.Println(err)
+		return nil
 	}
+	return nil
+
+}
+
+func retry() {
+	fmt.Println("Retrying...")
+
+	totalError := 9
+	for totalError > 0 {
+		totalError = 0
+		cmd := exec.Command("kubeadm", "certs", "renew", "apiserver-kubelet-client")
+		err := printOut(cmd)
+		if err != nil {
+			totalError ++
+		}
+
+
+		cmd = exec.Command("kubeadm", "certs", "renew", "apiserver")
+		err = printOut(cmd)
+		if err != nil {
+			totalError ++
+		}
+
+		cmd = exec.Command("kubeadm", "certs", "renew", "admin.conf")
+		err = printOut(cmd)
+		if err != nil {
+			totalError ++
+		}
+
+		fmt.Println(totalError)
+		// time.Sleep(5 * time.Second)
+
+	}
+
+
+
+
+}
+
+func main()  {
+	cmd := exec.Command("mv", "/etc/kubernetes/pki/ca.crt", "/etc/kubernetes/pki/ca.crt.bk")
+	printOut(cmd)
+	cmd = exec.Command("mv", "/etc/kubernetes/pki/ca.key", "/etc/kubernetes/pki/ca.key.bk")
+	printOut(cmd)
+
+	cmd = exec.Command("kubeadm", "init", "phase", "certs", "ca")
+	_ = printOut(cmd)
+
+	retry()
+
+
+
+
+
 }
